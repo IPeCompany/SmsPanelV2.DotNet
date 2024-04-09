98e1cdcab9a83326ca8853d62bbde6e5be3cbe13
SaeedGz98 <saeed.gholamzadeh198@gmail.com>
2024-04-09 16:39:19 +0330

    feat: add access-denied exception handling

--- IPE.SmsIrClient/Extensions/HttpRequestExtensions.cs
+++ IPE.SmsIrClient/Extensions/HttpRequestExtensions.cs
@@ -54,6 +54,8 @@ namespace IPE.SmsIrClient.Extensions
             {
                 case 401:
                     throw new UnauthorizedException(baseResponse.Status, baseResponse.Message);
+                case 403:
+                    throw new AccessDeniedException(baseResponse.Status, baseResponse.Message);
                 case 400:
                     throw new LogicalException(baseResponse.Status, baseResponse.Message);
                 case 429:
